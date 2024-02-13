edd32bc3c027cb846c932d12e6277abb5b020361
nick-stuckless <156607156+nick-stuckless@users.noreply.github.com>
2024-02-13 17:42:11 -0500

    second commit

--- pastebin_api.py
+++ pastebin_api.py
@@ -5,7 +5,7 @@ https://pastebin.com/doc_api
 import requests
 
 PASTEBIN_API_POST_URL = 'https://pastebin.com/api/api_post.php'
-API_DEV_KEY = 'Put your API key here'
+API_DEV_KEY = 'cVtzLy2H6hrZ3onDXNcw0Ex6dM7m7N-N'
 
 def post_new_paste(title, body_text, expiration='N', listed=True):
     """Posts a new paste to PasteBin
@@ -20,5 +20,23 @@ def post_new_paste(title, body_text, expiration='N', listed=True):
         str: URL of new paste, if successful. Otherwise None.
     """    
     # TODO: Function body
+    params = {
+        "api_dev_key": API_DEV_KEY,
+        "api_option": "paste",
+        "api_paste_code": body_text,
+        "api_paste_name": title,
+        "api_paste_expire_date": expiration,
+        "api_paste_private": 0 if listed else 1
+    }
+    print("Creating new paste ...", end="")
+    resp = requests.post(PASTEBIN_API_POST_URL, params=params)
+
+    if resp.status_code == requests.codes.ok:
+        print(" Success!")
+        return resp.text
+    else:
+        print(" Failure : ")
+        print (f"{resp.status_code} {resp.reason} ({resp.text})")
+        return 
     # Note: This function will be written as a group 
     return
